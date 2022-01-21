f585ed4b8d4b47699d09fc3d206088be637efe66
Ramajeyam-clayfin <ramajeyam@clayfin.com>
2022-01-21 16:11:42 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
 import LoadingButton from '@mui/lab/LoadingButton';
 import SaveIcon from '@mui/icons-material/Save';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditSharpIcon from '@mui/icons-material/EditSharp';
+import Box from '@mui/material/Box';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DesktopDatePicker from '@mui/lab/DatePicker';
 import './style.css';
 
+const CssTextField = styled(TextField)({
+  backgroundColor: 'white',
+});
+
 const App = () => {
-  const [date, setdate] = useState('');
+  const [date, setdate] = useState();
   const [time, settime] = useState('');
   const [notes, setnotes] = useState('');
-  const [num, setNum] = useState(1);
+  const [num, setNum] = useState(0);
+  let happyemoji = 'https://img.icons8.com/emoji/2x/smiling-face-with-halo.png';
+  let sademoji = 'https://img.icons8.com/emoji/2x/downcast-face-with-sweat.png';
+  let lovelyemoji =
+    'https://img.icons8.com/emoji/2x/smiling-face-with-heart-eyes.png';
+  const [emoji, setemoji] = useState('');
+  console.log(emoji);
 
   const [array, setarray] = useState([]);
   const [editid, setEditid] = useState(0);
@@ -28,6 +43,7 @@ const App = () => {
               date: map.date,
               time: map.time,
               num: map.num,
+              emoji: map.emoji,
             })
           : {
               id: map.id,
@@ -35,6 +51,7 @@ const App = () => {
               date: map.date,
               time: map.time,
               num: map.num,
+              emoji: map.emoji,
             }
       );
       setarray(updatedarray);
@@ -45,19 +62,28 @@ const App = () => {
 
     if (notes !== '') {
       setNum(num + 1);
-      addTask(notes, date, time, num);
+      addTask(notes, date, time, num, emoji);
       setnotes('');
       setdate('');
       settime('');
+      setemoji('');
     }
   };
 
-  const addTask = (notes, date, time, num) => {
+  const addTask = (notes, date, time, num, emoji) => {
     let d = date;
     let t = time;
-    let a = num;
+    let a = num + 1;
+    let e = emoji;
     const newTasks = [
-      { id: `${notes}-${Date.now()}`, notes, date: d, time: t, num: a },
+      {
+        id: `${notes}-${Date.now()}`,
+        notes,
+        date: d,
+        time: t,
+        num: a,
+        emoji: e,
+      },
       ...array,
     ];
     setarray(newTasks);
@@ -76,46 +102,103 @@ const App = () => {
   return (
     <div>
       <div>
-        <fieldset>
+        <fieldset className="input">
           <legend>
-            {' '}
             <h1>Diary Application</h1>
           </legend>
+
           <div className="text">
-            <form onSubmit={handleSubmit}>
+            <form>
               <label>Date : </label>
-              {/* <input
-              type="text"
-              value={date}
-              required={true}
-              // onChange={(e) => setdate(e.target.value)}
-            /> */}
               <input
                 type="date"
                 value={date}
-                required
-                onChange={(e) => setdate([e.target.value])}
+                // required
+                onChange={(e) => setdate(e.target.value)}
               />
+              {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DesktopDatePicker
+                  value={date}
+                  onChange={(e) => {
+                    setdate(e);
+                  }}
+                  renderInput={({ inputRef, inputProps, InputProps }) => (
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                      <input ref={inputRef} {...inputProps} />
+                      {InputProps?.endAdornment}
+                    </Box>
+                  )}
+                />
+              </LocalizationProvider> */}
               <br />
               <br />
               <label>Time : </label>
               <input
                 type="time"
                 value={time}
-                required
+                // required
                 onChange={(e) => settime(e.target.value)}
               />
               <br />
               <br />
-              <TextField
+              <CssTextField
                 label="Notes"
                 placeholder="Add a new task"
                 multiline
                 rows={5}
                 value={notes}
-                style={{ width: 260, backgroundColor: 'white' }}
+                className="textarea"
                 onChange={(e) => setnotes(e.target.value)}
               />
+              <br />
+              <br />
+              <label>How Are You Feeeling ?</label>
+              <br />
+              <br />
+              <div className="emoji">
+                <input
+                  type="image"
+                  src={happyemoji}
+                  width="35px"
+                  value="happy"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setemoji('😇');
+                  }}
+                />
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <input
+                  type="image"
+                  src={sademoji}
+                  width="35px"
+                  value="sad"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setemoji('😓');
+                  }}
+                />
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <input
+                  type="image"
+                  src={lovelyemoji}
+                  width="35px"
+                  value="lovely"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setemoji('😍');
+                  }}
+                />
+                <br />
+              </div>
+              {/* <button   value="happy" >
+                
+              </button>
+              <button  value="sad">
+                {sad}
+              </button>
+              <button  value="lovely">
+                {lovely}
+              </button> */}
               <br /> <br />
               <LoadingButton
                 startIcon={<SaveIcon />}
@@ -123,7 +206,7 @@ const App = () => {
                 type="submit"
                 onClick={handleSubmit}
               >
-                {editid ? 'Edit' : 'Save'}
+                {editid ? 'Update' : 'Add'}
               </LoadingButton>
               <br />
               <br />
@@ -135,21 +218,24 @@ const App = () => {
           <legend>
             <h3>Notes:</h3>
           </legend>
-          {array.map((map) => (
-            <>
-              <span key={map.id}>
-                <div style={{ fontSize: 35 }}>
-                  {map.num}.{'  '} {map.notes}
-                  {'  '}
-                  <EditSharpIcon onClick={() => handleEdit(map.id)} />{' '}
-                  <DeleteForeverIcon onClick={() => handleDelete(map.id)} />
+          {array.length
+            ? array.map((map, index) => (
+                <>
+                  <span key={index}>
+                    <div style={{ fontSize: 35 }}>
+                      {map.emoji}&nbsp;&nbsp; {map.notes}
+                      &nbsp;&nbsp;
+                      <EditSharpIcon onClick={() => handleEdit(map.id)} />
+                      &nbsp;&nbsp;
+                      <DeleteForeverIcon onClick={() => handleDelete(map.id)} />
+                      <br />
+                    </div>
+                    &nbsp;&nbsp; {map.date} - {map.time}
+                  </span>
                   <br />
-                </div>
-                {'  '} {map.date} - {map.time}
-              </span>
-              <br />
-            </>
-          ))}
+                </>
+              ))
+            : 'Add Notes to show ...'}
         </fieldset>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { StyledEngineProvider } from '@mui/material/styles';
+import App from './App';
 
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StyledEngineProvider injectFirst>
+    <App />
+  </StyledEngineProvider>,
+  document.getElementById('root')
+);
